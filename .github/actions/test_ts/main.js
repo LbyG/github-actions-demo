d1b6bf480b36e73bd3af6981572e3e249c3fc4b1
LbyG <604431363@qq.com>
2024-08-21 22:08:21 +0800

    add python sleep 1s

--- .github/actions/test_ts/main.js
+++ .github/actions/test_ts/main.js
@@ -7,16 +7,16 @@ console.log(`para1 is ${para1}, para2 is ${para2}`);
 // 以下是本地执行的代码
 console.log(`11111111111`);
 
-const pwd = require('child_process').exec('python src/main.py');
+const main_sleep1 = require('child_process').exec('python src/main_sleep1.py');
 
-pwd.stdout.on('data', (data) => {
-  console.log(`stdout: ${data}`);
+main_sleep1.stdout.on('data', (data) => {
+  console.log(`main_sleep1.stdout: ${data}`);
 });
 
-pwd.stderr.on('data', (data) => {
-  console.log(`stderr: ${data}`);
+main_sleep1.stderr.on('data', (data) => {
+  console.log(`main_sleep1.stderr: ${data}`);
 });
 
-pwd.on('close', (code) => {
-  console.log(`child process exited with code ${code}`);
+main_sleep1.on('exit', (code) => {
+  console.log(`main_sleep1 child process exited with code ${code}`);
 });
---
+++ src/main_sleep1.py
@@ -0,0 +1,7 @@
+import time
+
+
+print("hello world")
+print("sleep 1s start")
+time.sleep(1)
+print("sleep 1s finished")
