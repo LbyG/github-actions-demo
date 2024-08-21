644e5c2c4869e0d6ee8b7d77434b9d10610399fc
LbyG <604431363@qq.com>
2024-08-21 22:19:22 +0800

    modify order.

--- .github/actions/test_ts/main.js
+++ .github/actions/test_ts/main.js
@@ -5,23 +5,6 @@ let para2 = process.env['INPUT_PARA2'];
 console.log(`para1 is ${para1}, para2 is ${para2}`);
 
 // 以下是本地执行的代码
-console.log(`11111111111`);
-
-const main_sleep1 = require('child_process').exec('python src/main_sleep1.py');
-
-main_sleep1.stdout.on('data', (data) => {
-  console.log(`main_sleep1.stdout: ${data}`);
-});
-
-main_sleep1.stderr.on('data', (data) => {
-  console.log(`main_sleep1.stderr: ${data}`);
-});
-
-main_sleep1.on('exit', (code) => {
-  console.log(`main_sleep1 child process exited with code ${code}`);
-});
-
-// 以下是本地执行的代码
 console.log(`2222222222`);
 
 const main_sleep2 = require('child_process').exec('python src/main_sleep2.py');
@@ -36,4 +19,21 @@ main_sleep2.stderr.on('data', (data) => {
 
 main_sleep2.on('exit', (code) => {
   console.log(`main_sleep2 child process exited with code ${code}`);
+});
+
+// 以下是本地执行的代码
+console.log(`11111111111`);
+
+const main_sleep1 = require('child_process').exec('python src/main_sleep1.py');
+
+main_sleep1.stdout.on('data', (data) => {
+  console.log(`main_sleep1.stdout: ${data}`);
+});
+
+main_sleep1.stderr.on('data', (data) => {
+  console.log(`main_sleep1.stderr: ${data}`);
+});
+
+main_sleep1.on('exit', (code) => {
+  console.log(`main_sleep1 child process exited with code ${code}`);
 });
