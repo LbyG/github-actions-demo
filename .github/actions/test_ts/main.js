a816db3053b63ccbd83009f897baf464b9dd8559
LbyG <604431363@qq.com>
2024-08-21 21:53:16 +0800

    add log

---
+++ .github/actions/test_ts/main.js
@@ -0,0 +1,8 @@
+// 获取环境变量，所有传给github action的参数一律大写，且需要加上INPUT_前缀，此处是github规定的
+let para1 = process.env['INPUT_PARA1'];
+let para2 = process.env['INPUT_PARA2']; 
+// 输出到环境中
+console.log(`para1 is ${para1}, para2 is ${para2}`);
+
+// 以下是本地执行的代码
+console.log(`11111111111`);
